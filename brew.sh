cb06e64d9216c84478882b7671374677ef84638e
Kamil Pluta <detfis@gmail.com>
2016-11-18 15:58:30 +0100

    add some autocompletions

--- brew.sh
+++ brew.sh
@@ -15,4 +15,11 @@ brew install tmux
 brew install z
 brew install neovim/neovim/neovim
 
+brew tap homebrew/completions
+brew install homebrew/completions/mix-completion
+brew install homebrew/completions/ruby-completion
+brew install homebrew/completions/rails-completion
+brew install homebrew/completions/rake-completion
+brew install homebrew/completions/bundler-completion
+
 exit 0
